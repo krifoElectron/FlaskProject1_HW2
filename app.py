8b53aa5c80de5775bc22db936f3685d127cf6d8a
Gordeev Arsenii <Gordeev.A.Vladim@sberbank.ru>
2020-08-16 20:01:06 +0300

    HW 2

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-from functools import reduce
-from itertools import chain
 from data import title, subtitle, description, departures, tours
 
 app = Flask(__name__)
@@ -8,28 +6,26 @@ app = Flask(__name__)
 departure_list = [{'departure': departure[0], 'departure_display_name': departure[1]} for departure in
                   departures.items()]
 
-tour_list = reduce(
-    lambda prev, t: prev + [dict(chain.from_iterable(d.items() for d in ({'id': t[0]}, t[1])))], tours.items(),
-    [])
+tour_list = [{'id': tour_id, **tour} for tour_id, tour in tours.items()]
 
 
 @app.route('/')
 def render_main():
     return render_template('index.html', title=title, subtitle=subtitle, description=description, tour_list=tour_list,
-                           departure_list=departure_list, role='jjj', admin='asdf')
+                           departure_list=departure_list)
 
 
 @app.route('/departures/<departure>/')
 def render_departures(departure):
     departure_display_name = departures[departure][2:]
-    filtered_tour_list = list(filter(lambda tour: tour['departure'] == departure, tour_list))
+    filtered_tour_list = [tour for tour in tour_list if tour['departure'] == departure]
 
     number_of_tours = len(filtered_tour_list)
-    price_list = reduce(lambda prev, tour: prev + [tour['price']], filtered_tour_list, [])
+    price_list = [tour['price'] for tour in filtered_tour_list]
     min_price = min(price_list)
     max_price = max(price_list)
 
-    night_list = reduce(lambda prev, tour: prev + [tour['nights']], filtered_tour_list, [])
+    night_list = [tour['nights'] for tour in filtered_tour_list]
     min_nights = min(night_list)
     max_nights = max(night_list)
 
@@ -39,14 +35,14 @@ def render_departures(departure):
                            min_nights=min_nights, max_nights=max_nights)
 
 
-@app.route('/tours/<tour_id>')
+@app.route('/tours/<int:tour_id>')
 def render_tour(tour_id):
-    tour = tours[int(tour_id)]
+    tour = tours[tour_id]
 
     name = tour['title']
     number_of_starts = int(tour['stars'])
     to = tour['country']
-    from_where = departures[tour['departure']]
+    from_where = f'и{departures[tour["departure"]][1:]}'
     nights = tour['nights']
     picture = tour['picture']
 
@@ -58,4 +54,4 @@ def render_tour(tour_id):
                            picture=picture, tour_description=tour_description, price=price)
 
 
-app.run(debug=True)
+app.run()
